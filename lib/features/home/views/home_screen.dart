d9664acb66f6eef7745b2e47f3a51e1c11596e77
Sana Murtaza <sanamurtaza845@gmail.com>
2026-08-23 12:34:00 -0700

    resolve error

--- lib/features/home/views/home_screen.dart
+++ lib/features/home/views/home_screen.dart
@@ -239,7 +239,7 @@ class HomeScreen extends GetView<HomeController> {
                                 Get.toNamed(
                                   AppRoutes.notifications,
                                 );
-                              }, userName: '',
+                              },
                             ),
 
                             const SizedBox(height: 16),
