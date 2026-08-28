e3b4f8bb3f9ab60cca71381266337538c1e6d69b
rehman1783 <abdulrehman78ali@gmail.com>
2026-08-29 01:03:07 +0500

    feat: integrate FVM for Flutter version management and configure iOS project environment

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\amc\Desktop\app development work\recipe_app"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\amc\Desktop\app development work\recipe_app\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=C:\Users\abdul\fvm\versions\stable"
+export "FLUTTER_APPLICATION_PATH=C:\projects\Innovex_Technologies\recipe_app"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\projects\Innovex_Technologies\recipe_app\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
