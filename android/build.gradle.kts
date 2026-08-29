b4d2555f200ef4297e49815a4d25b653d3136192
rehman1783 <abdulrehman78ali@gmail.com>
2026-08-29 19:33:16 +0500

    feat: configure initial Android manifest and project-level build gradle settings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,12 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
+    val newSubprojectBuildDir: Directory =
+        newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
     project.evaluationDependsOn(":app")
 }
 
